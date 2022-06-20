0fe78dbf91cbf915aeaa514cdc61c7f51c598426
Coolhatena <ingsc.geffroy@gmail.com>
2022-06-20 16:27:40 -0700

    Solve memory leak

--- src/componentes/PokeCard.js
+++ src/componentes/PokeCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import getCardData from "../helpers/getCardData";
 
 import "../css/PokeCard.css";
@@ -6,14 +6,21 @@ import "../css/PokeCard.css";
 export const PokeCard = ({ name, url }) => {
   const [img, setImg] = useState("");
   const [id, setId] = useState(null);
+  const [types, setTypes] = useState([])
   const [loading, setLoading] = useState(true);
 
-  getCardData(url).then((data) => {
-    setImg(data.sprites.front_default);
-    setId(data.id);
-    setLoading(false);
-  });
+  useEffect(() => {
+    getCardData(url).then((data) => {
+      console.log('Cargando')
+      setImg(data.sprites.front_default);
+      setId(data.id);
+      setTypes(types => types.concat([...data.types]));
+      setLoading(false);
+    });
+  },[url])
+  
 
+  
   return (
     <div key={id} className="card animate__fadeInUp">
       {(loading)?
